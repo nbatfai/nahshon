fb9310d0264ae885713e57329316d6a971053b48
Norbert Bátfai <batfai.norbert@inf.unideb.hu>
2015-11-22 20:56:40 +0100

    After the work on the networked chat server for Samu is done, now I have started to work on a network chat client.
        The two typical use cases may be the following:
    
        - a Samu program talks to another Samu program
        - a Samu program talks to himself
    
        What I'd really like, of course, is not to have to code this as follows:
    
        main()
        {
        ...
        if(random() < a_magic_number)
         Samu_starts_talk();
        ...
        }
    
        but he should do it of his own free will :) A similar suggestion has been already made in paper http://arxiv.org/abs/1511.02889:
    
        "This is also reinforced by the fact that the incremental learning was implemented in the main.cpp and not in a class of Samu. From a point of view of a programmer, it means that the incremental approach is not yet really part of Samu."
    
        In the case of starting conversations, it means Samu's free will must be based on his learning algorithm. We are going to focus on trying to find a solution to this challenge in the next edition of Samu.

---
+++ net.hpp
@@ -0,0 +1,304 @@
+#ifndef NET_HPP
+#define NET_HPP
+
+/**
+ * @brief Amminadab - "The son of Ram is Amminadab."
+ *
+ * @file net.hpp
+ * @author  Norbert Bátfai <nbatfai@gmail.com>
+ * @version 0.0.1
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2015 Norbert Bátfai, batfai.norbert@inf.unideb.hu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ * JACOB, https://github.com/nbatfai/jacob
+ *
+ * "The son of Isaac is Jacob." The project called Jacob is an experiment
+ * to replace Isaac's (GUI based) visual imagination with a character console.
+ *
+ * ISAAC, https://github.com/nbatfai/isaac
+ *
+ * "The son of Samu is Isaac." The project called Isaac is a case study
+ * of using deep Q learning with neural networks for predicting the next
+ * sentence of a conversation.
+ *
+ * SAMU, https://github.com/nbatfai/samu
+ *
+ * The main purpose of this project is to allow the evaluation and
+ * verification of the results of the paper entitled "A disembodied
+ * developmental robotic agent called Samu Bátfai". It is our hope
+ * that Samu will be the ancestor of developmental robotics chatter
+ * bots that will be able to chat in natural language like humans do.
+ *
+ */
+
+#include <cstring>
+#include <sstream>
+
+#include <boost/asio.hpp>
+
+enum SamuRole {SERVER, CLIENT};
+
+class Net
+{
+public:
+
+  Net()
+  {
+  }
+
+  ~Net()
+  {
+  }
+
+  void write_session ( std::string msg )
+  {
+
+                  std::cerr << "SW write_session "<< msg << std::endl;
+
+    char data[4096];
+
+    int length = std::sprintf ( data, "%s", msg.c_str() );
+    try
+      {
+        boost::asio::write ( client_socket_, boost::asio::buffer ( data, length ) );
+      }
+    catch ( std::exception& e )
+      {
+        std::cerr << "Ooops: " << e.what() << std::endl;
+      }
+  }
+
+  void write_msg ( std::string msg )
+  {
+
+                  std::cerr << "CW write_msg "<< msg << std::endl;
+
+    char data[4096];
+
+    int length = std::sprintf ( data, "%s", msg.c_str() );
+    try
+      {
+        boost::asio::write ( socket_, boost::asio::buffer ( data, length ) );
+      }
+    catch ( std::exception& e )
+      {
+        std::cerr << "Ooops: " << e.what() << std::endl;
+      }
+  }
+  
+  
+  void read_session ( void )
+  {
+    char data[4096];
+
+    try
+      {
+        for ( ;; )
+          {
+            boost::system::error_code err;
+            size_t length = client_socket_.read_some ( boost::asio::buffer ( data ), err );
+
+            if ( err == boost::asio::error::eof )
+              {
+                hasSession = false;
+                break;
+              }
+            else if ( err )
+              {
+                throw boost::system::system_error ( err );
+              }
+
+              role = SERVER;
+              
+              
+            if ( length )
+              {
+                std::string s ( data, data+length );
+                buf = s;
+                already_read_ = false;
+		
+                  std::cerr << "SR read_session "<< buf << std::endl;
+		
+		
+              }
+          }
+      }
+    catch ( std::exception& e )
+      {
+        std::cerr << "Ooooops: " << e.what() << std::endl;
+      }
+  }
+
+  void start_server ( unsigned short port )
+  {
+    std::thread t {&Net::server, this, port };
+    t.detach();
+  }
+
+  void server ( unsigned short port )
+  {
+    boost::asio::ip::tcp::acceptor acceptor ( io_service_,
+        boost::asio::ip::tcp::endpoint ( boost::asio::ip::tcp::v4(), port ) );
+
+    char data[4096];
+
+    for ( ;; )
+      {
+        boost::asio::ip::tcp::socket socket ( io_service_ );
+        acceptor.accept ( socket );
+
+        if ( !hasSession )
+          {
+            hasSession = true;
+            client_socket_ = std::move ( socket );
+            std::thread t {&Net::read_session, this };
+            t.detach();
+          }
+        else
+          {
+            int length = std::sprintf ( data, "Samu is talking with somebody else" );
+            try
+              {
+                buf.clear();
+                boost::asio::write ( socket, boost::asio::buffer ( data, length ) );
+              }
+            catch ( std::exception& e )
+              {
+                buf.clear();
+                std::cerr << "Ooooops: " << e.what() << std::endl;
+              }
+
+          }
+      }
+  }
+
+  void start_client ( const char * host, unsigned short port )
+  {
+
+    boost::asio::ip::tcp::resolver resolver ( io_service_ );
+    boost::asio::ip::tcp::resolver::query query ( boost::asio::ip::tcp::v4(), host, "2006" );
+    boost::asio::ip::tcp::resolver::iterator iterator = resolver.resolve ( query );
+
+    boost::asio::ip::tcp::socket socket ( io_service_ );
+    boost::asio::connect ( socket, iterator );
+
+    socket_ = std::move ( socket );    
+    
+    hasSession2 = true;
+    
+    char data[4096];
+
+    size_t length = std::sprintf ( data, "I am Samu" );
+    socket_.send ( boost::asio::buffer ( data, length ) );
+    
+  }
+    
+  std::string client_msg ( void )
+  {    
+    char data[4096];
+    
+    
+            boost::system::error_code err;
+            size_t length = socket_.read_some ( boost::asio::buffer ( data ), err );
+
+            if ( err == boost::asio::error::eof )
+              {
+                
+              }
+            else if ( err )
+              {
+                throw boost::system::system_error ( err );
+              }
+
+              
+              role = CLIENT;
+              
+            if ( length )
+              {
+                std::string s ( data, data+length );
+                buf2_ = s;
+                already_read2_ = false;
+		
+                  std::cerr << "CR client_msg "<< buf2_ << std::endl;
+		
+		
+return buf2_;		
+              }	
+	return "quit";
+  }
+
+
+  void cg_read ( void )
+  {
+    if ( buf.size() > 0 &&  !already_read_ /*( ch = wgetch ( shell_w ) ) != ERR*/ )
+      {
+        already_read_ = true;
+
+        std::string ret ( buf );
+        throw ret;
+
+      }
+  }
+
+  void cg_read2 ( void )
+  {
+    if ( buf2_.size() > 0 &&  !already_read2_ /*( ch = wgetch ( shell_w ) ) != ERR*/ )
+      {
+        already_read2_ = true;
+
+        std::string ret ( buf2_ );
+        throw ret;
+
+      }
+  }
+  
+  
+  bool has_session ( void ) const
+  {
+    return hasSession;
+  }
+  bool has_session2 ( void ) const
+  {
+    return hasSession2;
+  }
+
+  SamuRole get_role ( void ) const
+  {
+    return role;
+  }
+  
+
+private:
+  SamuRole role = SERVER;
+  bool hasSession {false};
+  bool already_read_ {false};
+  boost::asio::io_service io_service_;
+  boost::asio::ip::tcp::socket client_socket_ {io_service_};
+  boost::asio::ip::tcp::socket socket_ {io_service_};
+  std::string buf;
+
+  bool already_read2_ {false};
+  std::string buf2_;
+  bool hasSession2 {false};
+  
+  
+};
+
+#endif
---
+++ samu.cpp
@@ -0,0 +1,315 @@
+/**
+ * @brief JUDAH - Jacob is equipped with a text-based user interface
+ *
+ * @file samu.cpp
+ * @author  Norbert Bátfai <nbatfai@gmail.com>
+ * @version 0.0.1
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2015 Norbert Bátfai, batfai.norbert@inf.unideb.hu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ * JACOB, https://github.com/nbatfai/jacob
+ *
+ * "The son of Isaac is Jacob." The project called Jacob is an experiment
+ * to replace Isaac's (GUI based) visual imagination with a character console.
+ *
+ * ISAAC, https://github.com/nbatfai/isaac
+ *
+ * "The son of Samu is Isaac." The project called Isaac is a case study
+ * of using deep Q learning with neural networks for predicting the next
+ * sentence of a conversation.
+ *
+ * SAMU, https://github.com/nbatfai/samu
+ *
+ * The main purpose of this project is to allow the evaluation and
+ * verification of the results of the paper entitled "A disembodied
+ * developmental robotic agent called Samu Bátfai". It is our hope
+ * that Samu will be the ancestor of developmental robotics chatter
+ * bots that will be able to chat in natural language like humans do.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "samu.hpp"
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <ncurses.h>
+
+
+#ifdef NETCHAT
+
+Net Samu::net;
+
+void Samu::NetworkCaregiverShell ( void )
+{
+
+  std::string cmd_prefix = "cmd";
+
+  int sleep {0};
+
+  if ( sleep_ )
+    sleep = sleep_after_ + 1;
+
+  int prev_sec {0};
+  for ( ; run_ ; )
+    {
+
+      try
+        {
+
+          net.cg_read();
+
+          if ( ++sleep > sleep_after_ )
+            {
+              if ( state == NETWORK )
+                {
+                  std::cerr << "Isaac went to sleep." << std::endl;
+#ifdef DISP_CURSES
+                  disp.log ( "I went to sleep." );
+#endif
+                  sleep_ = true;
+                  state = SLEEP;
+                }
+              sleep = sleep_after_ + 1;
+
+            }
+          else if ( state == NETWORK )
+            {
+              std::cerr << sleep << " *** " << std::flush;
+
+              int sec = ( sleep * read_usec_ ) / ( 1000*1000 );
+              if ( sec != prev_sec )
+                {
+                  int after = ( sleep_after_ * read_usec_ ) / ( 1000*1000 );
+
+                  std::stringstream sleep_after;
+
+                  sleep_after << "I will go to sleep after ";
+                  sleep_after << ( after-sec );
+                  sleep_after <<  " seconds";
+
+#ifdef DISP_CURSES
+                  disp.log ( sleep_after.str() );
+#endif
+                  prev_sec = sec;
+                }
+            }
+        }
+      catch ( std::string line )
+        {
+
+          if ( state != TERMINAL )
+            {
+              if ( sleep_ )
+                {
+                  std::cerr << "Isaac is awake now." << std::endl;
+#ifdef DISP_CURSES
+                  disp.log ( "I am awake now." );
+#endif
+                }
+              sleep_ = false;
+              sleep = 0;
+              state = NETWORK;
+
+	      disp.role = 0;
+	disp.ui();
+
+
+              if ( !line.compare ( 0, cmd_prefix.length(), cmd_prefix ) )
+                {
+                  std::string readCmd {"cmd read"};
+
+                  size_t f = line.find ( readCmd );
+                  if ( f != std::string::npos )
+                    {
+                      f = f+readCmd.length() +1;
+                      if ( f < line.length() )
+                        {
+                          std::string fname = line.substr ( f );
+                          set_training_file ( fname );
+                        }
+                    }
+                  else
+                    NextCaregiver();
+                }
+              else
+                {
+                  std::istringstream iss ( line );
+                  SPOTriplet t;
+                  iss >> t;
+
+                  SPOTriplets tv;
+                  tv.push_back ( t );
+
+                  try
+                    {
+                      //sentence ( -1, line );
+                      triplet ( -1000, tv );
+                    }
+                  catch ( const char* err )
+                    {
+                      std::cerr << err << std::endl;
+#ifdef DISP_CURSES
+                      disp.log ( err );
+#endif
+                    }
+                }
+            }
+        }
+
+      usleep ( read_usec_ );
+
+    }
+
+  run_ = false;
+}
+
+#else
+
+void Samu::NetworkCaregiverShell ( void )
+{
+  for ( ; run_ ; )
+    usleep ( read_usec_ );
+
+  run_ = false;
+}
+
+#endif
+
+#ifdef DISP_CURSES
+Disp Samu::disp;
+
+void Samu::FamilyCaregiverShell ( void )
+{
+  std::string cmd_prefix = "cmd";
+
+  int sleep {0};
+
+  if ( sleep_ )
+    sleep = sleep_after_ + 1;
+
+  int prev_sec {0};
+  for ( ; run_ ; )
+    {
+
+      try
+        {
+
+          disp.cg_read();
+
+          if ( ++sleep > sleep_after_ )
+            {
+              if ( state == TERMINAL )
+                {
+                  std::cerr << "Isaac went to sleep." << std::endl;
+                  disp.log ( "I went to sleep." );
+                  sleep_ = true;
+                  state = SLEEP;
+                }
+              sleep = sleep_after_ + 1;
+
+            }
+          else if ( state == TERMINAL )
+            {
+              std::cerr << sleep << " +++" << std::flush;
+
+              int sec = ( sleep * read_usec_ ) / ( 1000*1000 );
+              if ( sec != prev_sec )
+                {
+                  int after = ( sleep_after_ * read_usec_ ) / ( 1000*1000 );
+
+                  std::stringstream sleep_after;
+
+                  sleep_after << "I will go to sleep after ";
+                  sleep_after << ( after-sec );
+                  sleep_after <<  " seconds";
+
+                  disp.log ( sleep_after.str() );
+                  prev_sec = sec;
+                }
+            }
+        }
+      catch ( std::string line )
+        {
+          if ( state != NETWORK )
+            {
+
+              if ( sleep_ )
+                {
+                  std::cerr << "Isaac is awake now." << std::endl;
+                  disp.log ( "I am awake now." );
+                }
+              sleep_ = false;
+              sleep = 0;
+              state = TERMINAL;
+
+              if ( !line.compare ( 0, cmd_prefix.length(), cmd_prefix ) )
+                {
+                  std::string readCmd {"cmd read"};
+
+                  size_t f = line.find ( readCmd );
+                  if ( f != std::string::npos )
+                    {
+                      f = f+readCmd.length() +1;
+                      if ( f < line.length() )
+                        {
+                          std::string fname = line.substr ( f );
+                          set_training_file ( fname );
+                        }
+                    }
+                  else
+                    NextCaregiver();
+                }
+              else
+                {
+                  try
+                    {
+                      sentence ( -1, line );
+                    }
+                  catch ( const char* err )
+                    {
+                      std::cerr << err << std::endl;
+                      disp.log ( err );
+                    }
+                }
+            }
+        }
+      usleep ( read_usec_ );
+
+    }
+
+  run_ = false;
+}
+
+#else
+
+void Samu::FamilyCaregiverShell ( void )
+{
+  for ( ; run_ ; )
+    usleep ( read_usec_ );
+
+  run_ = false;
+}
+
+#endif
